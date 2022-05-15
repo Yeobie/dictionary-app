29905edadec8dea974c040ebf7eb3a5a60b4d4d8
Yeobie <konoha.shoujo@gmail.com>
2022-05-15 12:13:56 +0200

    added phonetics and conditional rendering for synonyms

--- src/Meanings.js
+++ src/Meanings.js
@@ -2,24 +2,45 @@ import React from "react";
 import Synonyms from "./Synonyms";
 
 export default function Meanings(props) {
-  return (
-    <div className="Meanings">
-      <h4> {props.meanings.partOfSpeech}</h4>
-      {props.meanings.definitions.map(function (definitions, index) {
-        return (
-          <div key={index}>
-            <p>
-              {" "}
-              <strong> Definition:</strong>
-              {definitions.definition}
-              <br />
-              <em> {definitions.example}</em>
-            </p>
-            <Synonyms synonyms={definitions.synonyms} />
-          </div>
-        );
-      })}
-      <Synonyms synonyms={props.meanings.synonyms} />
-    </div>
-  );
+  if (props.meanings.definitions.synonyms) {
+    return (
+      <div className="Meanings">
+        <h4> {props.meanings.partOfSpeech}</h4>
+        {props.meanings.definitions.map(function (definitions, index) {
+          return (
+            <div key={index}>
+              <p>
+                {" "}
+                <strong> Definition:</strong>
+                {definitions.definition}
+                <br />
+                <em> {definitions.example}</em>
+              </p>
+              <Synonyms synonyms={definitions.synonyms} />
+            </div>
+          );
+        })}
+      </div>
+    );
+  } else {
+    return (
+      <div className="Meanings">
+        <h4> {props.meanings.partOfSpeech}</h4>
+        {props.meanings.definitions.map(function (definitions, index) {
+          return (
+            <div key={index}>
+              <p>
+                {" "}
+                <strong> Definition:</strong>
+                {definitions.definition}
+                <br />
+                <em> {definitions.example}</em>
+              </p>
+            </div>
+          );
+        })}
+        <Synonyms synonyms={props.meanings.synonyms} />
+      </div>
+    );
+  }
 }
