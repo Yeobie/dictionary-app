96a99dbd0a2a9c72b8fa004c2119c31a3a76452c
Yeobie <konoha.shoujo@gmail.com>
2022-05-20 12:45:37 +0200

    added link to Netlify

--- src/App.js
+++ src/App.js
@@ -11,10 +11,23 @@ function App() {
         <footer className="Credits">
           <small>
             Created by{" "}
-            <a href="https://github.com/Yeobie/dictionary-app">
+            <a
+              href="https://github.com/Yeobie/dictionary-app"
+              target="_blank"
+              rel="noreferrer"
+              title="My Github repository"
+            >
               Ilaria Vilardi
             </a>{" "}
-            with React.
+            with React and{" "}
+            <a
+              href="https://app.netlify.com/sites/friendly-stardust-5aa51e/deploys"
+              target="_blank"
+              rel="noreferrer"
+              title="My Netlify"
+            >
+              hosted on Netlify.
+            </a>
           </small>
         </footer>
       </div>
